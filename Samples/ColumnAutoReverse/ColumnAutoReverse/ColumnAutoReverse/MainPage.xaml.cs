29af6bbed5ae00f5e92cdb8a5afa2d684187afd5
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-09-02 09:54:57 +0530

    Add files via upload

---
+++ Samples/ColumnAutoReverse/ColumnAutoReverse/ColumnAutoReverse/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ColumnAutoReverse
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //open picker dialog
+            date.IsOpen = !date.IsOpen;
+        }
+    }
+}
