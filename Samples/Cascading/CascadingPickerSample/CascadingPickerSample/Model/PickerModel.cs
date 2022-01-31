aa17a045fe43101b0e307114eb321f1ca1018848
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2022-01-31 10:45:11 +0530

    Add files via upload

---
+++ Samples/Cascading/CascadingPickerSample/CascadingPickerSample/Model/PickerModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CascadingPickerSample
+{
+    public class PickerModel
+    {
+        private string countries;
+
+        public string Countries
+        {
+            get { return countries; }
+            set { countries = value; }
+        }
+
+
+        private string countryCode;
+
+        public string CountryCode
+        {
+            get { return countryCode; }
+            set { countryCode = value; }
+        }
+
+        private string states;
+
+        public string States
+        {
+            get { return states; }
+            set { states = value; }
+        }
+
+        private string stateCode;
+
+        public string StateCode
+        {
+            get { return stateCode; }
+            set { stateCode = value; }
+        }
+    }
+}
